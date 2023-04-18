50ab4d0a044671e9199f1d6487270afa1f109248
Kodeeshwari Solanki <kodeeshwarisolanki@kodeeshwarisair.home>
2023-04-18 12:55:32 -0400

    first commit

---
+++ WeatherToWear/WeatherToWear/Model/UserModel.swift
@@ -0,0 +1,41 @@
+//
+//  UserModel.swift
+//  WeatherToWear
+//
+//  Created by Kodeeshwari Solanki on 2023-04-15.
+//
+
+import Foundation
+
+class UserModel : Codable{
+    
+    var fullName : String?
+    var email : String?
+    var phone : String?
+    var gender : Int?
+    var city : String?
+    var password : String?
+    
+    init(fullname : String,email : String,
+         phone : String, gender : Int, city : String,
+         password : String) {
+        self.fullName = fullname
+        self.email = email
+        self.phone = phone
+        self.gender = gender
+        self.city = city
+        self.password = password
+    }
+    
+    static func decode( json : [String:Any]!) -> UserModel? {
+
+        let decoder = JSONDecoder()
+        do{
+            let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+            let object = try decoder.decode(UserModel.self, from: data)
+            return object
+        } catch{
+        }
+        return nil
+    }
+}
---
+++ WeatherToWear/WeatherToWear/Source/Segue.swift
@@ -0,0 +1,14 @@
+//
+//  Segue.swift
+//  WeatherToWear
+//
+//  Created by Kodeeshwari Solanki on 2023-04-13.
+//
+
+import Foundation
+
+enum Segue{
+    static let toLoginViewController = "toLoginViewController"
+    static let toRegisterViewController = "toRegisterViewController"
+    static let toMainViewController = "toMainViewController"
+}
